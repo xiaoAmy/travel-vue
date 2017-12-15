98fc7edae4989f4533c7098ebf5f7f2704c41e92
Amyxiao <959433125@qq.com>
2017-12-15 14:47:08 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <p>
+      <router-link to="/">首页</router-link>
+      <router-link to="/about">关于我们</router-link>
+      <router-link to="/service">服务项目</router-link>
+      <router-link to="/news">聚城新闻</router-link>
+      <router-link to="/case">聚城案例</router-link>
+      <router-link to="/contact">联系我们</router-link>
+    </p>
+    <router-view></router-view>
+    <v-footer></v-footer>
+</div>
+</template>
+
+<script>
+import Vheader from '@/components/header/header';
+import Vfooter from '@/components/footer/footer';
+export default {
+  components: {
+       'v-header': Vheader,
+       'v-footer': Vfooter
+   }
+};
+</script>
+
+<style>
+#app {
+  width: 500px;
+  margin: auto;
+}
+</style>
